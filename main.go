605357184c1bd5ca7ee8a981b20687aa908f01fd
liangkai <lion0418@163.com>
2019-12-30 17:49:27 +0800

    fix package
    Signed-off-by: liangkai <lion0418@163.com>

--- main.go
+++ main.go
@@ -2,13 +2,10 @@ package main
 
 import (
 	"github.com/novliang/compass/aboriginal"
-	"github.com/novliang/compass/app"
 	"github.com/novliang/compass/compass"
 )
 
 func main() {
 	c := compass.New()
-	c.Run(aboriginal.NewServerByConfig(aboriginal.AboriginalConfig{
-		Routers: app.RouterInjection(),
-	}), "192.168.1.224", "8098")
+	c.Run(aboriginal.NewServer())
 }
